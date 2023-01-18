76e583a3467d8348e2d64bcaa2c5962133abccbe
KsanaSal <oasalivon@gmail.com>
2023-01-18 23:59:03 +0200

    Change schema contact and added contact pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,13 @@ const { HttpError } = require("../helpers");
 
 const getAll = async (req, res, next) => {
     try {
-        const contacts = await Contact.find();
+        const { _id: owner } = req.user;
+        const { page = 1, limit = 7 } = req.query;
+        const skip = (page - 1) * limit;
+        const contacts = await Contact.find(
+            { owner },
+            { skip, limit }
+        ).populate("owner", "name email");
         res.json({ status: "success", code: 200, data: { result: contacts } });
     } catch (error) {
         next(error);
@@ -31,7 +37,8 @@ const getById = async (req, res, next) => {
 
 const addContact = async (req, res, next) => {
     try {
-        const result = await Contact.create(req.body);
+        const { _id: owner } = req.user;
+        const result = await Contact.create({ ...req.body, owner });
         res.status(201).json({
             status: "success",
             code: 201,
