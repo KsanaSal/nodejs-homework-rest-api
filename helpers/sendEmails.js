3b8f9a8db1d346db137a3da8d6c55c4954843292
KsanaSal <oasalivon@gmail.com>
2023-02-01 22:43:20 +0200

    change user schema, add sendEmail file in helpers

---
+++ helpers/sendEmails.js
@@ -0,0 +1,14 @@
+const sendGridMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+const { SENDGRID_API_KEY } = process.env;
+
+sendGridMail.setApiKey(SENDGRID_API_KEY);
+
+const sendEmails = async (data) => {
+    const email = { ...data, from: "ksanasal@ukr.net" };
+    await sendGridMail.send(email);
+    return true;
+};
+
+module.exports = sendEmails;
